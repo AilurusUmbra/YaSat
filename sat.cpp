3216ff39afb3ac22c6c905adb488585ae976670d
ysliu <ailurus@Ailurus.localdomain>
2018-11-18 17:15:21 +0800

    add duplicate check

--- sat.cpp
+++ sat.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 #include"parser.h"
 
 using namespace std;
@@ -23,21 +24,72 @@ void printSAT(CNF& f, int rst){
   // if SAT, print variables;
   if(rst){
     cout<<"v ";
-
-
+    for(auto& lit : f.literals){
+      std::cout<<lit<<' ';           
+    }
     cout<<"0\n";
   }
   return; 
 }
 
 // BCP 
+void assignment(CNF& f, int l){
+
+  for(auto& cl : f.clauses){
+
+    // check for (a v a')
+    vector<int>::iterator pos_it;
+    pos_it = find (cl.begin(), cl.end(), l);
+
+    vector<int>::iterator neg_it;
+    neg_it = find (cl.begin(), cl.end(), -l);
+    
+    if (pos_it != cl.end() && neg_it != cl.end()){ 
+        printSAT(f, UNSAT);
+        exit(0);
+    }
+
+    for(auto& lit : cl){
+      if(lit==l){
+        cout<<"find it!\n";
+      }
+    }
+  }
+
+}
+
 void unitRule(CNF& f){
+
+  // for empty CNF
   if(f.clauses.empty()){
     printSAT(f, SAT);
     return;
   }
 
+  for(auto& cl : f.clauses){
+    
+    // find unit clause
+    if(cl.size()==1){
+      cout<<"unit: "<<cl[0]<<endl;
 
+      // set literal = 1 if unit clause is positive,
+      //             = 0 if unit clause is negative.
+      bool pos_clause = (cl[0]>0); 
+      int lit = pos_clause ? cl[0] : -cl[0];
+      f.literals[lit] = pos_clause ? 1 : 0; 
+      
+      // apply the assignment each clause 
+      assignment(f, lit);
+    }
+
+
+  }
+
+  cout<<"literals: ";
+  for(auto& lit : f.literals){
+    std::cout<<lit<<' ';           
+  }
+  cout<<endl;
 } 
     // unit clause find
       // implication
@@ -62,11 +114,10 @@ int main(int argc, char* argv[]){
     }                                    
     std::cout<<"\n";                     
   }                                      
-  std::cout<<"read file successfully\n"; 
   cout<<f.clauses.size()<<endl;
   
-  f.literals.resize(maxVarIndex, -1);   // 1:true, 0:false, -1: not assigned 
-  f.literal_freq.resize(maxVarIndex, 0);
+  f.literals.resize(maxVarIndex+1, -1);   // 1:true, 0:false, -1: not assigned 
+  f.literal_freq.resize(maxVarIndex+1, 0);
   
   // BCP
   unitRule(f);
